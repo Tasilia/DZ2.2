195e0390bcf74fe283478023ff2326d8c1f75e32
Anastasia Tsygankova <tsygnas@gmail.com>
2022-09-12 13:41:54 +0300

    Changed by comments

--- src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTest.java
@@ -3,6 +3,8 @@ package ru.netology.selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -12,29 +14,35 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardDeliveryTest {
     @BeforeEach
-    void openURL(){
+    void openURL() {
         open("http://localhost:9999");
+    }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
     void testSuccessMessage() {
         $x("//*[@placeholder='Город']").setValue("Москва");
-        LocalDate dateAfterThreeDays = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        $x("//*[@placeholder='Дата встречи']").setValue("dateAfterThreeDays.format(formatter)");
+        String planningDate = generateDate(3);
+        $x("//*[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.COMMAND, "A"),
+                Keys.BACK_SPACE);
+        $x("//*[@placeholder='Дата встречи']").setValue(planningDate);
         $x("//*[@name='name']").setValue("Юрий Гагарин");
         $x("//*[@name='phone']").setValue("+77777777777");
         $x("//span[@class='checkbox__box']").click();
         $x("//*[contains(text(), \"Забронировать\")]").click();
-        $x("//div[contains(text(), \"Успешно!\")]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(visible);
     }
 
     @Test
-    void testCalendar() {
-        LocalDate dateAfterSevenDays = LocalDate.now().plusDays(7);
-        int day = dateAfterSevenDays.getDayOfMonth();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String data = dateAfterSevenDays.format(formatter);
+    void testCalendarAndCity() {
+        $x("//*[@placeholder='Город']").setValue("Ка");
+        $x("//span[text()='Казань']").click();
+        String planningDate = generateDate(7);
+        String day = planningDate.substring(0, 2);
         $(".icon_name_calendar").click();
         if ($x("//td[@data-day][text()=" + day + "]").isDisplayed()) {
             $x("//td[@data-day][text()=" + day + "]").click();
@@ -42,13 +50,11 @@ public class AppCardDeliveryTest {
             $x("//div[@data-step=\"1\"]").click();
             $x("//td[@data-day][text()=" + day + "]").click();
         }
-        $x("//input[@value='" + data + "']").shouldBe(visible);
-    }
-
-    @Test
-    void testCity() {
-        $x("//*[@placeholder='Город']").setValue("Ка");
-        $x("//span[text()='Казань']").click();
-        $x("//*[@placeholder='Город'][@value='Казань']").shouldBe(visible);
+        $x("//*[@name='name']").setValue("Юрий Гагарин");
+        $x("//*[@name='phone']").setValue("+77777777777");
+        $x("//span[@class='checkbox__box']").click();
+        $x("//*[contains(text(), \"Забронировать\")]").click();
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
